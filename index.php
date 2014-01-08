a89d820aa1cd9225836db892e4fd6f09a0e9f7dc
Austin Matzko <austin@pressedcode.com>
2014-01-07 22:07:56 -0500

    Initial commit of spruced handmade landing page

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>spruced handmade</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width">
+		<meta name="author" content="spruced handmade">
+		<meta name="keywords" content="spruced handmade,jewelry,design" />
+
+		<link rel="stylesheet" href="/assets/css/normalize.css">
+		<link rel="stylesheet" href="/assets/css/main.css">
+		<script src="/assets/scripts/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+		<section id="wrapper">
+			<header itemscope itemtype="http://schema.org/Organization" id="main-name">
+				<h1 itemprop="name"><a href="/">spruced handmade</a></h1>
+			</header>
+		</section><!-- #wrapper -->
+		<footer>
+		</footer>
+	</body>
+</html>
+
